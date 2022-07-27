4487419f785605899e0376103f50c85c52d63e03
Keunwoo Choi <4292508+toeholdrj@users.noreply.github.com>
2022-07-27 18:13:04 -0400

    use yt_dlp to resolve duration and timing issue

--- audioset_dl/__init__.py
+++ audioset_dl/__init__.py
@@ -4,51 +4,25 @@ import os
 
 import pandas as pd
 from tqdm import tqdm
-from youtube_dl import YoutubeDL
-import time
+from yt_dlp import YoutubeDL
 
 
 def _download(x):
-
     ytid, start, end, out_dir,  = x
-    # if ytid != "b0RFKhbpFJA":
-    #     print('sleeping....')
-    #     time.sleep(10)
-
     start_dt, end_dt = dt.timedelta(milliseconds=start), dt.timedelta(milliseconds=end)
     print(f"{'-' * 30} | {ytid} {start_dt} to {end_dt} | {'-' * 30}")
     ydl_opts = {
-        "outtmpl": f"{out_dir}/[%(id)s]-[{start}ms].%(ext)s",
-        # "format": "bestaudio/best",  # 오디오만 나와서 그렇지 구간 자르는거랑 개잘됨.
-        # "format": "bestvideo/best",  # 비디오만 나오지만 구간 잘 자름.
-        # "format": "bestvideo/best+bestaudio/best", # 비디오만 나옴. 구간 굿.
-        # "format": "bestvideo[ext=mp4]/best+bestaudio/best", # 비디오만 나옴. 구간 굿.
-        # "format": "(bestvideo[ext=mp4]/best)+(bestaudio/best)",  # 소리 나오지만 개판인거 생김
-        # "format": "(bestvideo[ext=mp4]/best)+(bestaudio[ext=m4a]/best)",  # 아아 마찬가지 아아아
-        "format": "(bestvideo[height<=640]/bestvideo[ext=mp4]/best)+(bestaudio[ext=webm]/best[height<=640])",
-        # "format": "bestvideo+bestaudio/best",
-        # "format": "135",
-        # "format": "135/bestvideo/best+bestaudio/best",
-        # "format": "(135[ext=mp4]/135/bestvideo/best)+(bestaudio/best)",  # it works. but some of thme has audio sync issue
-
+        "outtmpl": f"{out_dir}/[%(id)s]-[{start//1000}-{end//1000}].%(ext)s",
+        "format": "bestaudio[ext=webm]/bestaudio/best",
+        # "format": "bestaudio/best",
         "external_downloader": "ffmpeg",
-        "external_downloader_args": ["-ss", f"{str(start_dt)}", "-t", f"0:00:10.00", "-loglevel", "info"],
-        # "external_downloader_args": ["-ss", f"{str(start_dt)}.00", "-to", f"{str(end_dt)}.00", "-loglevel", "info"],
-        # "external_downloader_args": ["-ss", str(start_dt), "-to", str(end_dt), "-loglevel", "panic"],
-
+        "external_downloader_args": ["-ss", str(start_dt), "-to", str(end_dt), "-loglevel", "panic"],
         "postprocessors": [
             {
-                # "key": "FFmpegExtractAudio",
-                # "preferredcodec": "wav",
-                'key': 'FFmpegVideoConvertor',
-                'preferedformat': 'mp4',  # one of avi, flv, mkv, mp4, ogg, webm
-                # "postprocessor-args": f" -ss {str(start_dt)} -to {str(end_dt)} ",  #  안됨. 엄청빨리 루프가 돌아가고 다운로드가 안됨.
-
-                # "codec": "copy",
-
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "wav",
             }
         ],
-        # "postprocessor_args": ["-ss", str(start_dt), "-to", str(end_dt)],  # 이건 먹히지 않음.
         "quiet": True,
         "no_mtime": True,
     }
@@ -62,8 +36,7 @@ def _download(x):
 
 
 def download_ps(ytid, st_list, ed_list, save_path, desc=None):
-    # with mp.Pool(processes=mp.cpu_count() // 2) as pool, tqdm(total=len(ytid), desc=desc) as pbar:
-    with mp.Pool(processes=4) as pool, tqdm(total=len(ytid), desc=desc) as pbar:
+    with mp.Pool(processes=mp.cpu_count() // 3) as pool, tqdm(total=len(ytid), desc=desc) as pbar:
         for _ in tqdm(pool.imap(_download, zip(ytid, st_list, ed_list, [save_path] * len(ytid)))):
             pbar.update()
 
