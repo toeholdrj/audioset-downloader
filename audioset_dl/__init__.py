4f23b69a88cfe3324ac8d815f517d55d160b1cd3
Keunwoo Choi <4292508+toeholdrj@users.noreply.github.com>
2022-08-01 11:04:44 -0400

    apply black

--- audioset_dl/__init__.py
+++ audioset_dl/__init__.py
@@ -10,7 +10,12 @@ import subprocess
 
 
 def _download_video_shell(x):
-    ytid, start, end, out_dir,  = x
+    (
+        ytid,
+        start,
+        end,
+        out_dir,
+    ) = x
 
     start_dt, end_dt = dt.timedelta(milliseconds=start), dt.timedelta(milliseconds=end)
     ydl_opts = {
@@ -18,19 +23,21 @@ def _download_video_shell(x):
         "format": "(bestvideo[height<=640]/bestvideo[ext=webm]/best)+(bestaudio[ext=webm]/best[height<=640])",
         "postprocessors": [
             {
-                'key': 'FFmpegVideoConvertor',
-                'preferedformat': 'mp4',  # one of avi, flv, mkv, mp4, ogg, webm
+                "key": "FFmpegVideoConvertor",
+                "preferedformat": "mp4",  # one of avi, flv, mkv, mp4, ogg, webm
             }
         ],
         "quiet": True,
         "no-mtime": True,
     }
-    yturl = f'https://youtube.com/watch?v={ytid}'
+    yturl = f"https://youtube.com/watch?v={ytid}"
     section_opt = f"*{start_dt}-{end_dt}"
-    cmd = f'yt-dlp -f "{ydl_opts["format"]}" {yturl} ' \
-          f'--download-sections {section_opt} ' \
-          f'--quiet ' \
-          f'--output "{ydl_opts["outtmpl"]}"'
+    cmd = (
+        f'yt-dlp -f "{ydl_opts["format"]}" {yturl} '
+        f"--download-sections {section_opt} "
+        f"--quiet "
+        f'--output "{ydl_opts["outtmpl"]}"'
+    )
     try:
         time.sleep(0.3)
         subprocess.run(cmd, shell=True, timeout=100)
@@ -41,13 +48,25 @@ def _download_video_shell(x):
 
 
 def _download_audio(x):
-    ytid, start, end, out_dir,  = x
+    (
+        ytid,
+        start,
+        end,
+        out_dir,
+    ) = x
     start_dt, end_dt = dt.timedelta(milliseconds=start), dt.timedelta(milliseconds=end)
     ydl_opts = {
         "outtmpl": f"{out_dir}/[%(id)s]-[{start//1000}-{end//1000}].%(ext)s",
         "format": "bestaudio[ext=webm]/bestaudio/best",
         "external_downloader": "ffmpeg",
-        "external_downloader_args": ["-ss", str(start_dt), "-to", str(end_dt), "-loglevel", "panic"],
+        "external_downloader_args": [
+            "-ss",
+            str(start_dt),
+            "-to",
+            str(end_dt),
+            "-loglevel",
+            "panic",
+        ],
         "postprocessors": [
             {
                 "key": "FFmpegExtractAudio",
@@ -66,23 +85,38 @@ def _download_audio(x):
         pass
 
 
-def download_ps(ytid, st_list, ed_list, save_path, target, num_processes=None, desc=None):
-    with mp.Pool(processes=num_processes) as pool, tqdm(total=len(ytid), desc=desc) as pbar:
-        if target == 'audio':
-            for _ in tqdm(pool.imap(_download_audio, zip(ytid, st_list, ed_list, [save_path] * len(ytid))), total=len(ytid)):
+def download_ps(
+    ytid, st_list, ed_list, save_path, target, num_processes=None, desc=None
+):
+    with mp.Pool(processes=num_processes) as pool, tqdm(
+        total=len(ytid), desc=desc
+    ) as pbar:
+        if target == "audio":
+            for _ in tqdm(
+                pool.imap(
+                    _download_audio,
+                    zip(ytid, st_list, ed_list, [save_path] * len(ytid)),
+                ),
+                total=len(ytid),
+            ):
                 pbar.update()
-        elif target == 'video':
-            for _ in tqdm(pool.imap(_download_video_shell, zip(ytid, st_list, ed_list, [save_path] * len(ytid)))):
+        elif target == "video":
+            for _ in tqdm(
+                pool.imap(
+                    _download_video_shell,
+                    zip(ytid, st_list, ed_list, [save_path] * len(ytid)),
+                )
+            ):
                 pbar.update()
         else:
-            raise NotImplementedError(f'target {target} is not implemented yet.')
+            raise NotImplementedError(f"target {target} is not implemented yet.")
 
 
 def _select_id(ytid, percent_from: int, percent_to: int):
     total = len(ytid)
-    idx_from = int(total * (percent_from / 100.))
-    idx_to = int(total * (percent_to / 100.))
-    return ytid[idx_from : idx_to]
+    idx_from = int(total * (percent_from / 100.0))
+    idx_to = int(total * (percent_to / 100.0))
+    return ytid[idx_from:idx_to]
 
 
 def dl_audioset_strong(save_path, split, args):
@@ -99,7 +133,15 @@ def dl_audioset_strong(save_path, split, args):
     ytid = _select_id(ytid, percent_from, percent_to)
     start_time = segment_id.str[12:].astype(int)
     end_time = start_time + 10000
-    download_ps(ytid, start_time, end_time, path, target, num_processes, desc=f"dl_{split}_strong")
+    download_ps(
+        ytid,
+        start_time,
+        end_time,
+        path,
+        target,
+        num_processes,
+        desc=f"dl_{split}_strong",
+    )
 
 
 def dl_audioset(save_path, split, args):
@@ -110,12 +152,19 @@ def dl_audioset(save_path, split, args):
 
     path = f"{save_path}/{split}"
     os.makedirs(path, exist_ok=True)
-    meta = pd.read_csv(f"audioset_dl/metadata/{split}_segments.csv", header=2, quotechar='"', skipinitialspace=True)
+    meta = pd.read_csv(
+        f"audioset_dl/metadata/{split}_segments.csv",
+        header=2,
+        quotechar='"',
+        skipinitialspace=True,
+    )
     ytid = meta["# YTID"]
     ytid = _select_id(ytid, percent_from, percent_to)
     start_time = (meta.start_seconds * 1000).astype(int)
     end_time = (meta.end_seconds * 1000).astype(int)
-    download_ps(ytid, start_time, end_time, path, target, num_processes, desc=f"dl_{split}")
+    download_ps(
+        ytid, start_time, end_time, path, target, num_processes, desc=f"dl_{split}"
+    )
 
 
 def dl_seglist(save_path, seglist_path, args):
@@ -130,4 +179,6 @@ def dl_seglist(save_path, seglist_path, args):
     ytid = segment_id.str[:11]
     start_time = segment_id.str[12:].astype(int)
     end_time = start_time + 10000
-    download_ps(ytid, start_time, end_time, path, target, num_processes, desc="dl_seglist")
+    download_ps(
+        ytid, start_time, end_time, path, target, num_processes, desc="dl_seglist"
+    )
